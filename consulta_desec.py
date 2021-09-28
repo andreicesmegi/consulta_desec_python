5cacae6b83e3208b0b21ea01fe3c65933b13c7f2
Andrei Cesmegi <79755533+andreicesmegi@users.noreply.github.com>
2021-09-28 12:16:19 -0300

    Agora sim! Voilá!
    Para que a impressão fosse feita corretamente, foi necessário adicionar o decode() ao final das variáveis. Desta feita, o resultado é impresso linha a linha na tela!!!

--- consulta_desec.py
+++ consulta_desec.py
@@ -15,6 +15,6 @@ s.close()
 s1 = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
 s1.connect((target,43))
 s1.send((sys.argv[1] + "\r\n").encode()) # Maneira 2 de concatenar textos e codifica-los
-response2 = s1.recv(1024)
+response2 = s1.recv(1024).decode()
 print = (response2)
 
